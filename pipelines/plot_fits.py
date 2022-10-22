b012b05c864dbde3dcd5501a11ed09736921e57a
Reijo Keskitalo <reijo.keskitalo@gmail.com>
2022-10-21 17:02:23 -0700

    Add script to plot reprocessing amplitudes

---
+++ pipelines/plot_fits.py
@@ -0,0 +1,119 @@
+import glob
+import os
+import sys
+
+import astropy.io.fits as pf
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+cumulative = True
+colors = [
+    "tab:blue",
+    "tab:orange",
+    "tab:green",
+    "tab:red",
+    "tab:purple",
+    "tab:brown",
+    "tab:pink",
+    "tab:grey",
+    "tab:olive",
+    "tab:cyan",
+    "black",
+    "gold",
+]
+
+if len(sys.argv) == 1:
+    raise RuntimeError(f"Usage: {sys.argv[0]} <output dir1> [<output_dir2>]")
+
+outdirs = sys.argv[1:]
+
+nrow1, ncol1 = 4, 4
+fig1 = plt.figure(figsize=[ncol1 * 4, nrow1 * 4])
+axes1 = None
+
+nrow2, ncol2 = 4, 8
+fig2 = plt.figure(figsize=[ncol2 * 4, nrow2 * 4])
+axes2 = None
+
+dets = None
+
+all_params = None
+params = None
+harmonics = None
+
+axes1 = []
+axes2 = []
+
+for outdir, linestyle in zip(outdirs, ["-", "--", ":"]):
+    param_fits = {}
+    harmonic_fits = {}
+
+    fnames = glob.glob(f"{outdir}/baselines_iter??.fits")
+    for fname in sorted(fnames):
+        print(fname)
+        hdulist = pf.open(fname, "readonly")
+        if all_params is None:
+            all_params = [hdu.header["extname"] for hdu in hdulist[1:]]
+            params = []
+            harmonics = []
+            for param in all_params:
+                if param.startswith("spin_harmonic"):
+                    harmonics.append(param)
+                else:
+                    params.append(param)
+        if dets is None:
+            dets = [col.name for col in hdulist[params[0]].columns]
+        for iparam, param in enumerate(params):
+            if param not in param_fits:
+                param_fits[param] = {}
+                for det in dets:
+                    param_fits[param][det] = []
+            for det in dets:
+                param_fits[param][det].append(hdulist[param].data[det])
+        for iparam, param in enumerate(harmonics):
+            if param not in harmonic_fits:
+                harmonic_fits[param] = {}
+                for det in dets:
+                    harmonic_fits[param][det] = []
+            for det in dets:
+                harmonic_fits[param][det].append(hdulist[param].data[det])
+
+    print(params)
+    #nparam = len(params)
+    #nharmonic = len(harmonics)
+
+    for iparam, param in enumerate(params):
+        if len(axes1) == iparam:
+            axes1.append(fig1.add_subplot(nrow1, ncol1, iparam + 1))
+        ax = axes1[iparam]
+        ax.set_title(param)
+        for det, color in zip(dets, colors):
+            values = param_fits[param][det]
+            if cumulative:
+                for i in range(1, len(values)):
+                    values[i] += values[i - 1]
+            values = np.hstack(values)
+            ax.plot(values, label=f"{det} {outdir}", color=color, linestyle=linestyle)
+
+    for iparam, param in enumerate(harmonics):
+        if len(axes2) == iparam:
+            axes2.append(fig2.add_subplot(nrow2, ncol2, iparam + 1))
+        ax = axes2[iparam]
+        ax.set_title(param)
+        for det, color in zip(dets, colors):
+            values = harmonic_fits[param][det]
+            if cumulative:
+                for i in range(1, len(values)):
+                    values[i] += values[i - 1]
+            values = np.hstack(values)
+            ax.plot(values, label=f"{det} {outdir}", color=color, linestyle=linestyle)
+
+axes1[-1].legend(bbox_to_anchor=(1,1), loc="upper left")
+axes2[-1].legend(bbox_to_anchor=(1,1), loc="upper left")
+
+fig1.savefig("parameter_fits.png")
+fig2.savefig("harmonic_fits.png")
+
+fig1.tight_layout()
+fig2.tight_layout()
